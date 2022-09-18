8c4f8c57382e72cbe94164c649a99bd5c47ab7c2
Krishnesh chaurasia <krishneshchaurasia1@gmail.com>
2022-09-18 17:38:23 +0530

    Initial commit

---
+++ Array/Array-Easy/KidsWithGreatestCandies.java
@@ -0,0 +1,15 @@
+class KidsWithGreatestCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = -1;
+        for(int a : candies)
+            max = Math.max(a, max);
+        
+        List<Boolean> res = new ArrayList<>();
+        
+        for(int i=0;i<candies.length;i++)
+           res.add((candies[i]+extraCandies>=max) ? true : false);
+        
+        return res;
+    }
+}
+
---
+++ Array/Array-Easy/KidsWithGreatestCandies2.java
@@ -0,0 +1,10 @@
+class KidsWithGreatestCandies2 {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max=Integer.MIN_VALUE;
+        List<Boolean> arrList=new ArrayList<>();
+        for(int i:candies) max=Math.max(i,max);
+        for(int i:candies) arrList.add(i+extraCandies>=max);
+        return arrList;
+        
+    }
+}
