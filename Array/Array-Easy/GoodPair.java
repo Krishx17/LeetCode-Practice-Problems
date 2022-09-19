e8624265c0116e0e6a123af0c23f1e75db79bc99
Krishnesh chaurasia <krishneshchaurasia1@gmail.com>
2022-09-19 12:52:53 +0530

    Initial commit.

---
+++ Array/Array-Easy/GoodPair.java
@@ -0,0 +1,15 @@
+class GoodPair {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] == nums[j] && i < j){
+                    count++;
+                }else{
+                    continue;
+                }
+            }
+        }
+        return count;
+    }
+}
